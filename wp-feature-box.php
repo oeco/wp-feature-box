c473ae5e5ddaf2f6d6b09a0aa1e37db4f0c4190b
Miguel Peixe <miguel@cardume.art.br>
2013-12-04 20:49:13 -0200

    fix license

--- wp-feature-box.php
+++ wp-feature-box.php
@@ -6,14 +6,15 @@ Description: A simple WordPress feature box plugin
 Version: 0.1.0
 Author: Miguel Peixe
 Author URI: http://ecolab.oeco.org.br/
-License: GPL2
+License: GPLv3
 */
 /*
 Copyright 2012 Miguel Peixe  (email : miguel@cardume.art.br)
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License, version 2, as 
-published by the Free Software Foundation.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -21,8 +22,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 if(!class_exists('WP_Feature_Box')) {
