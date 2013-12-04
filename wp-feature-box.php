686bff62ea810c05e5b68dbb63744aa06509234c
Miguel Peixe <miguel@cardume.art.br>
2013-12-04 20:44:09 -0200

    default slider set to last 6 items

--- wp-feature-box.php
+++ wp-feature-box.php
@@ -417,8 +417,13 @@ if(!class_exists('WP_Feature_Box')) {
          */
 		public function get_feature_box_slider($ids = array(), $options = array()) {
 			
-			if(empty($ids) || !is_array($ids))
-				return false;
+			if(empty($ids) || !is_array($ids)) {
+				$items = get_posts(array('post_type' => 'wp_feature_box', 'posts_per_page' => 6));
+				$ids = array();
+				foreach($items as $item) {
+					$ids[] = $item->ID;
+				}
+			}
 			
 			$settings = array(
 				'autorotate' => 0
