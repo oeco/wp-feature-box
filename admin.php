162f04fe3af831f4a776d7449779755a3ace086b
Miguel Peixe <miguel@cardume.art.br>
2013-12-04 16:46:35 -0200

    fix admin form

--- admin.php
+++ admin.php
@@ -13,29 +13,36 @@ if(!class_exists('WP_Feature_Box_Admin')) {
 		
 		var $settings_group = 'wfb_settings_group';
 		
+		var $settings_page = 'wfb-settings';
+		
 		function __construct() {
 			
 			add_action('admin_menu', array($this, 'add_plugin_page'));
 			add_action('admin_init', array($this, 'page_init'));
-			
+
 		}
 		
 		function add_plugin_page() {
-			add_submenu_page('edit.php?post_type=' . $this->post_type, __('Settings', 'wp-feature-box'), __('Settings', 'wp-feature-box'), 'manage_options', $this->settings_id, array($this, 'create_admin_page'));
+			add_submenu_page('edit.php?post_type=' . $this->post_type, __('Settings', 'wp-feature-box'), __('Settings', 'wp-feature-box'), 'manage_options', $this->settings_page, array($this, 'create_admin_page'));
 		}
 		
 		function create_admin_page() {
+			$this->store_data();
 			?>
 			<div class="wrap">
 				<?php screen_icon(); ?>
-				<h2><?php _e('Feature Box Settings', 'wp-feature-box'); ?></h2>
+				<h2><?php _e('Feature box settings', 'wp-feature-box'); ?></h2>
 				<form method="post">
 					<?php
 					// This prints out all hidden setting fields
 					settings_fields($this->settings_group);
-					do_settings_sections($this->settings_id);
+					do_settings_sections($this->settings_page);
+					submit_button(); 
 					?>
-					<?php submit_button(); ?>
+				</form>
+				<form method="post">
+					<input type="hidden" name="wfb_settings_reset" value="1" />
+					<?php submit_button(__('Restore default settings', 'wp-feature-box'), 'secondary'); ?>
 				</form>
 			</div>
 			<?php
@@ -43,7 +50,7 @@ if(!class_exists('WP_Feature_Box_Admin')) {
 		
 		function page_init() {
 			
-			register_setting($this->settings_group, $this->settings_id, array($this, 'check_data'));
+			//register_setting($this->settings_group, $this->settings_id, array($this, 'store_data'));
 			
 			/* 
 			 * Embed
@@ -53,26 +60,68 @@ if(!class_exists('WP_Feature_Box_Admin')) {
 				'wfb_embed',
 				__('Embed options', 'wp-feature-box'),
 				array($this, 'embed_info'),
-				$this->settings_id
+				$this->settings_page
 			);
 			
 			add_settings_field(
 				'allow_embed',
 				__('Allow embed', 'wp-feature-box'),
 				array($this, 'embed_allow_input'),
-				$this->settings_id,
+				$this->settings_page,
+				'wfb_embed'
+			);
+			
+			add_settings_field(
+				'default_embed_text',
+				__('Default footer text (embed credits)', 'wp-feature-box'),
+				array($this, 'embed_default_text_input'),
+				$this->settings_page,
+				'wfb_embed'
+			);
+			
+			add_settings_field(
+				'default_embed_link',
+				__('Default footer text link', 'wp-feature-box'),
+				array($this, 'embed_default_link_input'),
+				$this->settings_page,
 				'wfb_embed'
 			);
 			
 		}
 		
-		function check_data($input){
-			error_log('checking');
-			if(get_option('wfb_settings') === false)
-				add_option('wfb_settings', $input);
-			else
-				update_option('wfb_settings', $input);
+		function store_data($input = false) {
 			
+			if(isset($_REQUEST['wfb_settings_reset'])) {
+				delete_option($this->settings_id);
+				return;
+			}
+
+			$input = $input ? $input : $_POST[$this->settings_id];
+			
+			if(isset($input)) {
+				if(get_option($this->settings_id) === false)
+					add_option($this->settings_id, $input);
+				else
+					update_option($this->settings_id, $input);
+			}
+
+		}
+		
+		function get_options() {
+
+			$settings = array(
+				'allow_embed' => 1,
+				'default_embed_text' => __('View more at', 'wp-feature-box') . ' ' . get_bloginfo('name'),
+				'default_embed_link' => home_url('/')
+			);
+			
+			$options = get_option($this->settings_id);
+			
+			if($options)
+				return array_merge($settings, $options);
+			
+			return $settings;
+
 		}
 		
 		function embed_info() {
@@ -80,8 +129,25 @@ if(!class_exists('WP_Feature_Box_Admin')) {
 		}
 		
 		function embed_allow_input() {
-			$options = get_option('wfb_settings');
-			?><input type="checkbox" id="wfb_allow_embed" name="<?php echo $this->settings_id; ?>[allow_embed]" value="1" <?php if($option['allow_embed']) echo 'checked'; ?> /><?php
+			$options = $this->get_options();
+			?>
+			<input type="radio" id="wfb_allow_embed_yes" name="<?php echo $this->settings_id; ?>[allow_embed]" value="1" <?php if($options['allow_embed']) echo 'checked'; ?> /> <label for="wfb_allow_embed_yes"><?php _e('Yes', 'wp-feature-box'); ?></label>
+			<input type="radio" id="wfb_allow_embed_no" name="<?php echo $this->settings_id; ?>[allow_embed]" value="0" <?php if(!$options['allow_embed']) echo 'checked'; ?> /> <label for="wfb_allow_embed_no"><?php _e('No', 'wp-feature-box'); ?></label>
+			<?php
+		}
+		
+		function embed_default_text_input() {
+			$options = $this->get_options();
+			?>
+			<input type="text" id="wfb_default_embed_text" size="80" name="<?php echo $this->settings_id; ?>[default_embed_text]" value="<?php echo $options['default_embed_text']; ?>" />
+			<?php
+		}
+		
+		function embed_default_link_input() {
+			$options = $this->get_options();
+			?>
+			<input type="text" id="wfb_default_embed_link" size="80" name="<?php echo $this->settings_id; ?>[default_embed_link]" value="<?php echo $options['default_embed_link']; ?>" />
+			<?php
 		}
 		
 	}
